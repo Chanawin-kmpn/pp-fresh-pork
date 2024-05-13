4bff589d0540900450fe3e666b92ed69bb3e25d4
Upond <pond_chanawin@hotmail.com>
2024-05-13 14:27:30 +0700

    Update: update mock data for categories and products

---
+++ src/app/data/categories.js
@@ -0,0 +1,14 @@
+export const categories = [
+  {
+    category: 1,
+    name: "เนื้อหมู",
+  },
+  {
+    category: 2,
+    name: "เนื้อไก่",
+  },
+  {
+    category: 3,
+    name: "เนื้อวัว",
+  },
+];
---
+++ src/app/data/products.js
@@ -0,0 +1,173 @@
+export const products = [
+  {
+    productId: 1001,
+    categoryId: 1,
+    name: "สามชั้นสไลด์",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1002,
+    categoryId: 1,
+    name: "สันคอสไลด์",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1003,
+    categoryId: 1,
+    name: "สันนอกสไลด์",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1004,
+    categoryId: 1,
+    name: "เบคอน",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1005,
+    categoryId: 1,
+    name: "หมูหมักนุ่ม",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1006,
+    categoryId: 1,
+    name: "หมูหมักน้ำมันงา",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1007,
+    categoryId: 1,
+    name: "หมูหมักพริกไทยดำ",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1008,
+    categoryId: 1,
+    name: "หมูหมักน้ำผึ้ง",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1009,
+    categoryId: 1,
+    name: "หมูหมักบาร์บีคิว",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1010,
+    categoryId: 1,
+    name: "หมูหมักหม่าล่า",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1011,
+    categoryId: 1,
+    name: "หมูหมักนุ่มปรุงรส",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1012,
+    categoryId: 2,
+    name: "ไก่แซ่บ",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1013,
+    categoryId: 2,
+    name: "ไก่หมักน้ำมันงา",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1014,
+    categoryId: 2,
+    name: "ไก่หมักพริกไทยดำ",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1015,
+    categoryId: 2,
+    name: "ไก่หมักน้ำผึ้ง",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1016,
+    categoryId: 2,
+    name: "ไก่หมักบาร์บีคิว",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1017,
+    categoryId: 3,
+    name: "เนื้อหมักนุ่ม",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1018,
+    categoryId: 3,
+    name: "เนื้อหมักซอส",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+  {
+    productId: 1019,
+    categoryId: 3,
+    name: "เนื้อหมักพริกไทยดำ",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, ipsam accusantium perferendis obcaecati molestiae atque cupiditate ipsa quos provident quaerat voluptatibus pariatur architecto illo autem quibusdam? Nam quo quam laboriosam?",
+    productSize: 1000,
+    productImg: "https://fakeimg.pl/544x468/",
+  },
+];
