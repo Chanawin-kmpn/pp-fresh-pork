dc4ae127a4bb060971c484885fc603c464814ea9
Upond <pond_chanawin@hotmail.com>
2024-05-25 11:43:02 +0700

    Update: update align layout main section

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../main.css";
 function Main({ children }) {
   return (
-    <main className="main-bg flex flex-col items-center bg-white px-4 py-8 min-h-[401px] lg:flex-row lg:justify-center lg:min-h-[1498px] lg:rounded-lg lg:mx-20 lg:px-20 lg:py-16">
+    <main className="main-bg flex flex-col items-center bg-white px-4 py-8 min-h-[401px] lg:flex-row lg:justify-center lg:items-start lg:min-h-[1498px] lg:rounded-lg lg:mx-20 lg:px-20 lg:py-16">
       {children}
     </main>
   );
