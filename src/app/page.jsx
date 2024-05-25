47c0add6310564ac91c4e049d085ef9cbeb7d811
Upond <pond_chanawin@hotmail.com>
2024-05-25 11:37:29 +0700

    Update: complete homepage mobile responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,29 +4,32 @@ import Link from "next/link";
 import "./homepage.css";
 export default function Home() {
   return (
-    <main className="container flex flex-col justify-center items-center gap-16 mx-auto">
+    <div className="container flex flex-col justify-center items-center gap-16">
       <div className="relative">
-        <p className=" text-7xl leading-[1.1rem] font-bold text-gray5 bg-primary px-[56px] py-[170px]">
+        <p className="welcome-title top-0 -left-[34%] text-[4rem] font-bold text-primary leading-[1.1] tracking-wider lg:absolute lg:text-8xl">
+          WEL
+          <span className="text-[4rem] font-bold text-primary lg:text-gray5 lg:text-8xl">
+            COME
+          </span>
+        </p>
+        <p className="text-[3rem] leading-[1.1rem] font-bold text-gray5 bg-primary py-[5rem] lg:px-[56px] lg:py-[170px] lg:text-7xl">
           P.P. Fresh Pork
         </p>
-        <p className="welcome-title absolute top-0 -left-[34%] text-8xl font-bold text-primary leading-[1.1] tracking-wider">
-          WEL<span className="text-8xl font-bold text-gray5">COME</span>
-        </p>
-        <div className="absolute top-[80%] -right-[100px] flex flex-col gap-4">
-          <p className="animated-text text-6xl font-bold text-gray5 leading-[1.1] opacity-0">
+        <div className="flex flex-col gap-4 lg:absolute lg:top-[85%] lg:-right-[100px]">
+          <p className="animated-text text-[3rem] font-bold text-primary leading-[1.1] opacity-0 lg:text-6xl lg:text-gray5 lg:mx-0">
             Fresh
           </p>
-          <p className="animated-text text-6xl font-bold text-primary leading-[1.1] opacity-0">
+          <p className="animated-text left-1/4 text-[3rem] font-bold text-primary leading-[1.1] opacity-0 mx-[15%] lg:text-6xl lg:mx-0">
             Clean
           </p>
-          <p className="animated-text text-6xl font-bold text-primary leading-[1.1] opacity-0">
+          <p className="animated-text text-[3rem] font-bold text-primary leading-[1.1] opacity-0 mx-[25%] lg:text-6xl lg:mx-0">
             Quality
           </p>
-          <p className="animated-text text-6xl font-bold text-primary leading-[1.1] opacity-0">
+          <p className="animated-text text-[3rem] font-bold text-primary leading-[1.1] opacity-0 mx-[35%] lg:text-6xl lg:mx-0">
             Premium
           </p>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
